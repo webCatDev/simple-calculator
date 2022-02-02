1dff5dbe6e23ee77fa4b407b765bab28a511287f
unknown <webcatdev@gmail.com>
2022-02-02 16:45:28 +0300

    uploaded files

---
+++ calculator.js
@@ -0,0 +1,46 @@
+const numbers=document.getElementsByClassName("number")
+const operators=document.getElementsByClassName("operator")
+const calculation= document.getElementById("calculation")
+const paranthesis = document.getElementById("paranthesis");
+const equalsTo = document.getElementById("equals-to");
+const backspace = document.getElementById("backspace");
+const clear = document.getElementById("clear");
+
+
+
+for(const number of numbers){
+    number.addEventListener("click", ()=>{
+        calculation.textContent+=number.value
+    })
+}
+
+
+for (const operator of operators) {
+  operator.addEventListener("click", () => {
+    calculation.textContent += operator.value;
+  });
+}
+
+
+equalsTo.addEventListener("click",()=>{
+const result = eval(calculation.textContent);
+calculation.textContent=result
+})
+
+
+backspace.addEventListener("click", () => {
+    let text= calculation.textContent
+    text=text.substring(0,text.length-1)
+    calculation.textContent=text
+ 
+});
+
+clear.addEventListener("click", () => {
+  let text = calculation.textContent;
+  text = "";
+  calculation.textContent = text;
+});
+
+
+
+
