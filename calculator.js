63f293b304dc6bcd6db78e0a4a5a7ca836c5e7a5
unknown <webcatdev@gmail.com>
2022-02-03 03:14:24 +0300

    added styling

--- calculator.js
+++ calculator.js
@@ -7,7 +7,6 @@ const backspace = document.getElementById("backspace");
 const clear = document.getElementById("clear");
 
 
-
 for(const number of numbers){
     number.addEventListener("click", ()=>{
         calculation.textContent+=number.value
@@ -41,6 +40,12 @@ clear.addEventListener("click", () => {
   calculation.textContent = text;
 });
 
-
+calculation.addEventListener("keydown", (e)=>{
+  if(e.key==="Enter"){
+    e.preventDefault()
+    const result = eval(calculation.textContent).toFixed(2);
+    calculation.textContent = result;
+  }
+})
 
 
